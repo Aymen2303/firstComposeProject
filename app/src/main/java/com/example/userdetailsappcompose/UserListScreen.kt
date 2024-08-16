36d4aabedfaa03db4dff004e89b17078a6b447a7
Aymen2303 <aimenlalaibia@gmail.com>
2024-08-16 14:12:04 +0100

    Fixed Api problems

--- app/src/main/java/com/example/userdetailsappcompose/UserListScreen.kt
+++ app/src/main/java/com/example/userdetailsappcompose/UserListScreen.kt
@@ -1,33 +1,27 @@
 import android.util.Log
-import androidx.annotation.UiContext
 import androidx.compose.runtime.*
-import kotlinx.coroutines.launch
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.ui.text.style.TextAlign
 import com.example.userdetailsappcompose.CardUserDetails
 import com.example.userdetailsappcompose.network.RetrofitInstance
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 
 @Composable
 fun UserListScreen(modifier: Modifier = Modifier, retrofitInstance: RetrofitInstance) {
     val users = remember { mutableStateOf<List<User>>(emptyList()) }
 
     LaunchedEffect(Unit) {
-            try {
-                val getUsers = retrofitInstance.service.listUsers(10)
-                users.value = getUsers.results
-            } catch (e: Exception) {
-                e.printStackTrace()
-                Log.e("ApiError", "Error fetching users", e)
-            }
+        try {
+            val response = retrofitInstance.service.listUsers(10)
+            users.value = response.results
+            print("users are:${users.value}")
+        } catch (e: Exception) {
+            Log.e("ApiError", "Error fetching users", e)
+        }
     }
 
     if (users.value.isEmpty()) {
--- app/src/main/java/com/example/userdetailsappcompose/network/UserService.kt
+++ app/src/main/java/com/example/userdetailsappcompose/network/UserService.kt
@@ -2,15 +2,14 @@ package com.example.userdetailsappcompose.network
 
 import ApiResponse
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 
 interface UserService {
 
-    @GET("/")
+    @GET("?results=")
     suspend fun listUsers(
-        @Query("results") userSize: Int,
+        @Query("resultsNum") userSize: Int,
     ): ApiResponse
 
 }
