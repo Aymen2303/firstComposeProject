1d0204e6483b2eb34adfa8be315b74a0f0deada6
Feinate <mehdi.fullstackdev@gmail.com>
2024-08-27 17:36:18 +0100

    Cleaned Files + Changed Ui design of UserDetails Screen to match Figma File.

--- app/src/main/java/com/example/userdetailsappcompose/UserDetailsScreen.kt
+++ app/src/main/java/com/example/userdetailsappcompose/UserDetailsScreen.kt
@@ -5,10 +5,12 @@ import UserInformationsBox
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.material3.AlertDialogDefaults.shape
@@ -26,128 +28,146 @@ import coil.compose.rememberAsyncImagePainter
 import com.example.userdetailsappcompose.ui.theme.UserDetailsAppComposeTheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
+import com.example.userdetailsappcompose.Values.cornerShape
+import com.example.userdetailsappcompose.Values.paddingBody
+import com.example.userdetailsappcompose.Values.topAppBarHeight
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserDetailsScreen(
-    onBack: () -> Unit,
     name: String,
     email: String,
     dob: String,
     phone: String,
-    imageUrl: String
+    imageUrl: String,
+    onBack: () -> Unit
 ) {
     val painter = rememberAsyncImagePainter(model = imageUrl)
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                navigationIcon = {
-                    IconButton(onClick = onBack) {
-                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
-                    }
-                },
-                title = {
-                    Box(
-                        modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text ="User Details",
-                            style = TextStyle(
-                                fontSize = 32.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = colorResource(id = R.color.selected),
-                    titleContentColor = colorResource(id = R.color.white)
-                )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+            .padding(
+                top = WindowInsets.statusBars
+                    .asPaddingValues()
+                    .calculateTopPadding()
             )
-        },
-        content = { paddingValues ->
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    color = colorResource(id = R.color.selected),
+                    shape = RoundedCornerShape(
+                        topStart = cornerShape,
+                        topEnd = cornerShape
+                    )
+                )
+        ) {
             Box(
                 modifier = Modifier
-                    .background(color = colorResource(id = R.color.selected))
-                    .padding(paddingValues),
+                    .fillMaxWidth()
+                    .height(topAppBarHeight)
             ) {
-                Column(
+                IconButton(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 30.dp)
-                        .verticalScroll(rememberScrollState()),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .align(Alignment.CenterStart)
+                        .padding(start = 4.dp),
+                    onClick = onBack
                 ) {
+                    Icon(
+                        Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back",
+                        tint = Color.White
+                    )
+                }
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.Center),
+                    text = "User Details",
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                )
+            }
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        start = paddingBody,
+                        end = paddingBody,
+                        bottom = paddingBody + WindowInsets.navigationBars
+                            .asPaddingValues()
+                            .calculateBottomPadding()
+                    )
+                    .background(
+                        color = Color.White,
+                        shape = RoundedCornerShape(
+                            topStart = cornerShape - paddingBody,
+                            topEnd = cornerShape - paddingBody,
+                            bottomEnd = cornerShape - paddingBody,
+                            bottomStart = cornerShape - paddingBody
+                        )
+                    )
+                    .padding(top = 30.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                item {
                     Box(
                         modifier = Modifier
-                            .height(850.dp)
-                            .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
-                            .background(color = colorResource(id = R.color.white))
-                    ) {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            modifier = Modifier.padding(16.dp)
-                        ) {
-                            Spacer(modifier = Modifier.height(30.dp))
-
-                            Box(
-                                modifier = Modifier
-                                    .size(124.dp)
-                                    .graphicsLayer {
-                                        shadowElevation = 4.dp.toPx()
-                                        shape = RoundedCornerShape(18.dp)
-                                        clip = true
-                                        translationX = 2f
-                                        translationY = 4f
-                                    }
-                            ) {
-                                Image(
-                                    painter = painter,
-                                    contentDescription = "User profile picture",
-                                    contentScale = ContentScale.Crop,
-                                    modifier = Modifier
-                                        .fillMaxSize()
-                                        .clip(RoundedCornerShape(16.dp))
-                                )
+                            .size(124.dp)
+                            .graphicsLayer {
+                                shadowElevation = 4.dp.toPx()
+                                shape = RoundedCornerShape(18.dp)
+                                clip = true
+                                translationX = 2f
+                                translationY = 4f
                             }
-
-                            Spacer(modifier = Modifier.height(16.dp))
-
-                            Text(
-                                text = name,
-                                style = TextStyle(
-                                    fontSize = 18.sp,
-                                    fontWeight = FontWeight.Bold
-                                ),
-                                modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
-                            )
-
-                            Spacer(modifier = Modifier.height(50.dp))
-
-                            UserDobElevatedCard(
-                                dob,
-                                modifier = Modifier.align(Alignment.CenterHorizontally)
-                            )
-                            Spacer(modifier = Modifier.height(80.dp))
-
-                            UserInformationsBox(
-                                name = name,
-                                email = email,
-                                phoneNumber = phone
-                            )
-                        }
+                    ) {
+                        Image(
+                            painter = painter,
+                            contentDescription = "User profile picture",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .clip(RoundedCornerShape(16.dp))
+                        )
                     }
+                }
+                item { Spacer(modifier = Modifier.height(16.dp)) }
+                item {
+                    Text(
+                        text = name,
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold
+                        ),
+                        modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+                    )
+                    Spacer(modifier = Modifier.height(50.dp))
+                    UserDobElevatedCard(
+                        dob,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Spacer(modifier = Modifier.height(80.dp))
+                    UserInformationsBox(
+                        name = name,
+                        email = email,
+                        phoneNumber = phone
+                    )
                 }
             }
         }
-    )
+    }
 }
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 fun PreviewOfComponent() {
     UserDetailsAppComposeTheme {
@@ -160,5 +180,11 @@ fun PreviewOfComponent() {
             imageUrl = "https://randomuser.me/api/portraits/women/54.jpg"
         )
     }
+}
+
+internal object Values {
+    val topAppBarHeight = 70.dp
+    val paddingBody = 4.dp
+    val cornerShape = 20.dp
 }
 
